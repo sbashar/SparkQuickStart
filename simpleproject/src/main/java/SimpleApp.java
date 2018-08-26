8fc2437082674896ef5556fc5a178e84b0faa4d7
Saydul Bashar <tamimnoone@gmail.com>
2018-08-26 02:09:33 +0100

    Fixed the issue with Intellij complaining about filter ambiguity.

--- simpleproject/src/main/java/SimpleApp.java
+++ simpleproject/src/main/java/SimpleApp.java
@@ -1,3 +1,4 @@
+import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
 
@@ -7,8 +8,8 @@ public class SimpleApp {
         SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
         Dataset<String> logData = spark.read().textFile(logFile).cache();
 
-        long numAs = logData.filter(s -> s.contains("a")).count();
-        long numBs = logData.filter(s -> s.contains("b")).count();
+        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
+        long numBs = logData.filter((FilterFunction<String>) s -> s.contains("b")).count();
 
         System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
 
